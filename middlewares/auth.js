44faf2d55e607812979ac2c8d75ac29597ce6a1d
ChaitanyaBhake <138829875+ChaitanyaBhake@users.noreply.github.com>
2024-05-02 20:08:31 +0530

    Updated middleware for token issue

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 exports.auth = (req, res, next) => {
-    // Check authorization header
-    const token = req.cookies.token || req.body.token;
+     //Extracting Token from headers
+    const token = req.headers.authorization?.split(' ')[1];
 
     //if token is missing return no response
     if (!token) {
